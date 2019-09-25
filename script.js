e58fe78dd6698a23ac66712b68782ba2da4a50c5
Alex Sierra <alexandercsierra@gmail.com>
2019-09-25 16:06:31 -0400

    red background added bootstrap and jQuery

---
+++ script.js
@@ -0,0 +1,53 @@
+var rpsArr = ["ROCK", "PAPER", "SCISSORS"];
+var userChoice = "";
+var compChoice = "";
+var resultText = "";
+var outcome = document.getElementById("outcome");
+var winLoss = document.getElementById("winLoss");
+
+
+
+$("button").click(function(){
+  compChoice = randomChoice();
+  userChoice = $(this).text();
+  round(userChoice, compChoice);
+  $("#initialChoice").addClass("hide");
+  $("#result").removeClass("hide");
+  outcome.textContent = resultText;
+});
+
+$("#playAgain").click(function(){
+  winLoss.textContent = "";
+  outcome.textContent = "";
+  location.reload();
+});
+
+function randomChoice(){
+  var random = Math.floor(Math.random() * rpsArr.length);
+  return rpsArr[random];
+}
+
+function round (user, comp) {
+  if((user === "ROCK " && comp === "SCISSORS") || (user === "PAPER " && comp === "ROCK") || (user === "SCISSORS " && comp === "PAPER")) {
+    return win();
+  } else if ((user === "ROCK " && comp === "ROCK") || (user === "PAPER " && comp === "PAPER") || (user === "SCISSORS " && comp === "SCISSORS")) {
+    return draw();
+  } else {
+    return lose();
+  }
+}
+
+
+function win() {
+  resultText = userChoice + "beats " + compChoice;
+}
+
+function lose() {
+  resultText = compChoice + " beats " + userChoice;
+  winLoss.textContent = "You Lose!"
+}
+
+function draw() {
+  winLoss.textContent = "It\'s a draw."
+  resultText = "You both picked " + compChoice;
+}
